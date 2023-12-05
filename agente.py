aa3fdf3dca8b36b65511824a685dd2712aad14ec
LucyKirumi <130850937+LucyKirumi@users.noreply.github.com>
2023-12-05 20:59:07 +0000

    Adicionado alguns testes sobre grafos para as zonas

--- agente.py
+++ agente.py
@@ -1,12 +1,39 @@
 import time
 import gender_guesser.detector as gender
-
+import networkx as nx
+# !!!!!!!!!!!!!!! NAO TE ESQUEÇAS DE COLOCAR COMENTARIOS !!!!!!!!!!!!!!!
 pilha = []
 pilha_resp1 = []
 
 def identify_gender(name):
     d = gender.Detector()
     return d.get_gender(name)
+
+'''
+!!Esta parte que adicionei provavelmente vai ser modificada!!
+Isto só serve como base para ver se funciona
+Cenas para adicionar:
+- Atributos para cada nodo: tipo de zona, coordenadas
+- Forma de quando o robot descobre a zona atualizar o nodo
+'''
+# Ligacoes entre zonas
+G = nx.Graph()
+edge_list = [
+    # Corredor 1
+    (1, 0), (1, 2), (1, 5), (1, 3),
+    # Corredor 2
+    (2, 6), (2, 7),
+    # Corredor 3
+    (3, 4), (3, 12), (3, 13), (3, 14),
+    # Corredor 4
+    (4, 8), (4, 9), (4, 10), (4, 11)
+    ]
+G.add_edges_from(edge_list)
+
+'''
+Para encontrar o caminho mais curto o networkx já tem uma funcao implementada
+print(nx.shortest_path(G, 6, 11)) o 6 e 11 são exemplos de como funciona -> to-do: implementar isto no futuro adaptado à pergunta
+'''
 
 def work(posicao, bateria, objetos):
     time.time()
