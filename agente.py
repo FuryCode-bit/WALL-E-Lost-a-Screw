45cf967070d5d8ee9919cd1ee4480665ec1082d9
Fury <62396294+FuryCode-bit@users.noreply.github.com>
2023-12-31 17:09:25 +0000

    Update agente.py

--- agente.py
+++ agente.py
@@ -14,27 +14,27 @@ PERGUNTAS:
 4. DONE (I THINK)
 5. TODO
 6. DONE
-7. DONE - Needs Revision
+7. DONE (Check Calculus)
 8. TODO
 '''
  
 import time
 import networkx as nx
 from utils import p1, p6, ambiente as amb
-
+ 
 pilha = []
 encontros = []
  
 pilhaBateriaTempo = []
-
+ 
 # Variáveis para o grafo
 posAtual = 10 # Número da zona atual
 G = amb.criarGrafo()
 nodeAtual = G.nodes[posAtual]
-
+ 
 tempoDecorrido = time.time()
 tempo100 = time.time()
-
+ 
 # esta funcao nao devia de estar no p6?
 def updateListaBateriaTempo(bateria):
     global tempoDecorrido, tempo100, pilhaBateriaTempo
@@ -49,7 +49,7 @@ def updateListaBateriaTempo(bateria):
     if int(bateria) == 0:
         print("Bateria acabou!")
         # print(pilhaBateriaTempo)
-
+ 
 '''
 Esta função identifica cada objeto em que o WALL-E entra em contacto e vai inserindo numa pilha.
 Para identificar um humano é usado uma lista de prefixos com os títulos disponíveis. 
@@ -61,14 +61,14 @@ def work(posicao, bateria, objetos):
 	global tempo_final
 	global tempo_decorrido
  
-	global posatual
-	global nodeatual
+	global posAtual
+	global nodeAtual
  
 	# Mudança de Zona
 	if posicao[0] <= nodeAtual["coord"][0][0] or posicao[0] >= nodeAtual["coord"][1][0] \
 	or posicao[1] <= nodeAtual["coord"][0][1] or posicao[1] >= nodeAtual["coord"][1][1]:
 		posAtual, nodeAtual = amb.mudarZona(posicao, posAtual)
-	
+ 
 	person_prefixes = ['operário_', 'visitante_', 'supervisor_']
 	operario_prefix = 'operário_'
 	supervisor_prefix = 'supervisor_'
@@ -76,30 +76,22 @@ def work(posicao, bateria, objetos):
 	if objetos and isinstance(objetos, list) and len(objetos) == 1:
 		obj = objetos[0]
  		# Mudar Tipo de Zona
-		# Cada Node está estruturado da seguinte maneira:
-		# Node (
-		#  tipo: str	
-		#  maquinas: []	
-		#  operarios: []	
-		#  supervisores: []	
-		#  Coord: [[][]]
-		
-		# A implementação seguinte armazena cada objeto de acordo com o seu prefixo na lista correspondente
 		if obj.startswith("zona_") and amb.compTipoZona(nodeAtual, "sem identificação"):
 			nodeAtual["tipo"] = obj[len("zona_"):]
 			print("Mudança de tipo de zona para: ", nodeAtual["tipo"])
 		elif obj.startswith(maquina_prefix):
 			maquina_value = obj[len(maquina_prefix):]
 			if maquina_value not in nodeAtual["maquinas"]:
-				nodeAtual["maquinas"].append(obj[len(maquina_prefix):])
+				nodeAtual["maquinas"].append(maquina_value)
 		elif obj.startswith(operario_prefix):
 			operario_value = obj[len(operario_prefix):]
-			if maquina_value not in nodeAtual["operarios"]:
-				nodeAtual["operarios"].append(obj[len(operario_prefix):])
+			if operario_value not in nodeAtual["operarios"]:
+				nodeAtual["operarios"].append(operario_value)
+				print(nodeAtual)
 		elif obj.startswith(supervisor_prefix):
 			supervisor_value = obj[len(supervisor_prefix):]
-			if maquina_value not in nodeAtual["supervisores"]:
-				nodeAtual["supervisores"].append(obj[len(supervisor_prefix):])
+			if supervisor_value not in nodeAtual["supervisores"]:
+				nodeAtual["supervisores"].append(supervisor_value)
 		else:
 			for prefix in person_prefixes:
 				if obj.startswith(prefix) and obj not in pilha and obj[len(prefix):] not in pilha:
@@ -121,17 +113,16 @@ Esta função começa por verificar quantas pessoas de género masculino o WALL-
 Se apenas teve em contacto com uma (ou com nenhuma) ele indica que não possui informação suficiente para resolver o problema.
 Se tiver toda a informação necessária, vai indicar o nome da penúltima pessoa que encontrou!
 '''
-
 def resp1():
 	if len(encontros) >= 2:
 		penultimo = encontros[-2]
 		print("O penultimo elemento é: ", penultimo)
 	else:
 		print("Não existe informação suficiente")
-
+ 
 def resp2():
 	print("A zona", posAtual, "é do tipo", nodeAtual["tipo"])
-
+ 
 def resp3():
 	if amb.compTipoZona(nodeAtual, "empacotamento"):
 		print("Já estou na zona de empacotamento")
@@ -153,19 +144,23 @@ def resp4():
 			caminho = nx.shortest_path(G, posAtual, dest) # Encontrar caminho
 			distTotal = amb.distancia(caminho)
 			print("A distância desde a zona", posAtual, "até o laboratório", dest, "é", distTotal)
-
+ 
 def resp5():
 	pass
-
+ 
 def resp6():
 	# Chama a função para estimar o tempo necessário
-	tempo_estimado = p6.PrevisaoPorBateria(pilhaBateriaTempo)
+	tempo_estimado = p6.PrevisaoPorBateria(PilhaBateriaTempo)
  
 	# Exibe o tempo estimado para atingir o nível de bateria desejado
 	print(f"Tempo estimado para atingir 0% de bateria: {tempo_estimado:.2f} segundos")
-
+ 
 def resp7():
-	print(amb.probabildadeProximoSerSupervisor(G.nodes(data=True)))
-
+	# Qual a probabilidade da próxima pessoa que encontrares ser um supervisor?
+	try:
+		print(f"A probabilidade da próxima pessoa que encontrar ser um supervisor é {amb.probabildadePrximoSerSupervisor(G):.3f}")
+	except ZeroDivisionError:
+		print("Não existe informação suficiente no mundo conhecido!")
+		
 def resp8():
 	pass
